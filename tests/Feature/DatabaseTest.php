c51c927dc2402ef803449d3309aa7fec01d8cdf3
Athos Benther <athos@benther.tech>
2025-10-22 16:34:04 -0300

    updates database test

--- tests/Feature/DatabaseTest.php
+++ tests/Feature/DatabaseTest.php
@@ -1,11 +1,10 @@
 <?php
 
-use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\DB;
 
-describe('database', function () {
-    test('can read', function () {
-        $tables = DB::table('Customers')->get();
-        expect($tables instanceof Collection)->toBeTrue();
+describe('Database', function () {
+    test('can get PDO', function () {
+        $tables = DB::connection()->getPdo();
+        expect($tables instanceof \PDO)->toBeTrue();
     });
 });
