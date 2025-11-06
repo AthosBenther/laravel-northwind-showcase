033423e3f5a956afc7d2fdb8fbc54ebe3e9272ec
Athos Benther <athum@live.com>
2025-11-06 20:36:23 -0300

    Makes Scramble understand the results type

--- app/Http/Resources/OrderCollection.php
+++ app/Http/Resources/OrderCollection.php
@@ -4,16 +4,19 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\ResourceCollection;
+use App\Http\Resources\OrderResource;
 
 class OrderCollection extends ResourceCollection
 {
     /**
      * Transform the resource collection into an array.
-     *
-     * @return array<OrderResource>
      */
-    public function toArray(Request $request): array
+    public function toArray(Request $request)
     {
-        return parent::toArray($request);
+        return $this->collection->map(
+            function ($item) {
+                return new OrderResource($item);
+            }
+        );
     }
 }
